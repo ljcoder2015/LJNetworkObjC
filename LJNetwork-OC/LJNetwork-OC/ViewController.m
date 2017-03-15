67793e4c98ffa2ee009bf215eb0ad663daaea2d7
ljcoder <ljcoder@163.com>
2017-03-15 10:44:11 +0800

    接口测试

--- LJNetwork-OC/LJNetwork-OC/ViewController.m
+++ LJNetwork-OC/LJNetwork-OC/ViewController.m
@@ -13,6 +13,9 @@
 
 @property (strong, nonatomic) LJTestAPI *testAPI;
 
+@property (strong, nonatomic) UIButton *requestButton;
+@property (strong, nonatomic) UITextView *responsTextView;
+
 @end
 
 @implementation ViewController
@@ -30,12 +33,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.requestButton = ({
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+        button.titleLabel.font = [UIFont systemFontOfSize:14];
+        [button setTitle:@"Start Request" forState:UIControlStateNormal];
+        [button addTarget:self action:@selector(reqeust:) forControlEvents:UIControlEventTouchUpInside];
+        button;
+    });
+    [self.view addSubview:self.requestButton];
+    
+    [self.requestButton sizeToFit];
+    self.requestButton.center = self.view.center;
+    
+    self.responsTextView = ({
+        UITextView *textView = [[UITextView alloc] initWithFrame:CGRectMake(10, self.view.center.y+30, self.view.bounds.size.width-20, self.view.bounds.size.height-self.view.center.y-30)];
+        textView.layer.borderColor = [UIColor orangeColor].CGColor;
+        textView.layer.borderWidth = 1;
+        textView.editable = NO;
+        textView;
+    });
+    [self.view addSubview:self.responsTextView];
+    
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)reqeust:(UIButton *)button {
+    self.responsTextView.text = @"";
+    [self.testAPI loadData];
 }
 
 #pragma mark - LJRequestParametersDataSource
@@ -52,7 +75,8 @@
     
     if (manager == self.testAPI) {
         
-        NSLog(@"%@", responseObject);
+        NSLog(@"responseObject = %@", responseObject);
+        self.responsTextView.text = [NSString stringWithFormat:@"%@", responseObject];
     }
 }
 
